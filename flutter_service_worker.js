a9d6d0548f890e091da072e7d818487c7483b6c5
Nils ten Hoeve <nilstenhoeve@gmail.com>
2024-02-02 13:58:03 +0100

    Generated by meyn_lbh_simulation/tool/publish.dart

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "def73c443ae0e01fa8096a2f4a39131c",
-"/": "def73c443ae0e01fa8096a2f4a39131c",
-"main.dart.js": "81e4c725a8e37aa3e4cda13b9b6322fd",
+"index.html": "d5bf053dfd7fb840341cfd5188b027ab",
+"/": "d5bf053dfd7fb840341cfd5188b027ab",
+"main.dart.js": "90544b124026eddd0c4c42a80048024e",
 "manifest.json": "6d287312bacc34d7d5ca551685e95a14",
 "version.json": "a8a2ea4e1a38a2099dab772e171a7d05"};
 // The application shell files that are downloaded before a service worker can
