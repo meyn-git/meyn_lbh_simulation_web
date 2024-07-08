3e8195d47b988143654dea2eb3967491a6185b4d
Nils ten Hoeve <nilstenhoeve@gmail.com>
2024-07-08 09:59:29 +0200

    Generated by meyn_lbh_simulation/tool/publish.dart

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"bb10c5466638e963479ba5e64e601e42d1a434
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3939771884"
+    serviceWorkerVersion: "2160765656"
   }
 });
